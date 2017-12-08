b38c20193690c8af9033eea31909e49c4bd59db7
Erlend Lygre <erlend.tl2@gmail.com>
2017-12-08 12:32:24 +0100

    adding return dog button

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,8 @@
 class Dog < ApplicationRecord
   belongs_to :user
-
   has_many :bookings, dependent: :destroy
   has_many :ratings, dependent: :destroy
   has_attachment :photo
-
   validates :name, presence: true
   validates :species, presence: true
   validates :description, presence: true, length: { maximum: 100 }
